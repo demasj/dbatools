018173eb9c933a960698ec1642a3a7aec174f715
Demas Jaring <demas.jaring@vwe.nl>
2019-07-31 10:33:29 +0200

    Add CMS group DBADailyCheck

--- ErrorLog.ps1
+++ ErrorLog.ps1
@@ -1,5 +1,6 @@
-$cred = Get-Credential 
+$cred = Get-Credential
+
 $array = @("taking longer","Severity: 16","Severity: 17","Severity: 18","Severity: 19","Severity: 20","Severity: 21","Severity: 22","Severity: 23","Severity: 24","could not","failure","autogrow","killed","SqlDump","SQL Server is starting","victim")
 foreach ($search in $array) {
-	Get-DbaCmsRegServer "localhost\sql2017a" -Group NewMotive -SqlCredential $cred | Get-DbaErrorLog -LogNumber 0,1 -Text $search -SqlCredential $cred
+	Get-DbaCmsRegServer "localhost\sql2017a" -Group DBADailyCheck -SqlCredential $cred | Get-DbaErrorLog -LogNumber 0,1 -Text $search -SqlCredential $cred
 } Format-Table -AutoSize
